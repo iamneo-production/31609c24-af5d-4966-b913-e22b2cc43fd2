96c5cff7890d087021f81c6662cceb0ce97f67fe
chaithu-04 <chaitanyamudela@gmail.com>
2023-08-10 13:18:03 +0000

    added card component

--- unit-converter/src/components/mainComponents/unitConverter/UnitConverter.js
+++ unit-converter/src/components/mainComponents/unitConverter/UnitConverter.js
@@ -1,9 +1,10 @@
 import "./UnitConverter.css";
-
+import Card from "../../constants/card/Card";
 const UnitConverter = () => {
   return (
     <div className="app-body">
       <h1>Welcome to Unit - Converter</h1>
+      <Card/>
     </div>
   );
 };
