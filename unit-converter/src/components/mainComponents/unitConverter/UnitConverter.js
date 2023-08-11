7635e53911091a1ee90cd5fdf742ea6c5e34e395
shivssm <shivssm95@gmail.com>
2023-08-11 05:56:29 +0000

    Added Card to Home Page

--- unit-converter/src/components/mainComponents/unitConverter/UnitConverter.js
+++ unit-converter/src/components/mainComponents/unitConverter/UnitConverter.js
@@ -1,10 +1,20 @@
 import "./UnitConverter.css";
 import Cards from "../../constants/card/Cards";
+import { unitTypes } from "../../constants/ConstantJson";
+import { Row } from "reactstrap";
+
 const UnitConverter = () => {
   return (
     <div className="app-body">
       <h1>Welcome to Unit - Converter</h1>
-      <Cards/>
+
+      <div className="unitCards">
+        {unitTypes.map((item) => (
+          <div key={item.id}>
+            <Cards />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
