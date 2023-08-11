11017dd8cc255dbe3dcbb184aed0e0478ae36b9b
shivssm <shivssm95@gmail.com>
2023-08-11 04:41:52 +0000

    Added Constant Json

---
+++ unit-converter/src/components/constants/ConstantJson.js
@@ -0,0 +1,34 @@
+const unitTypes = [
+  {
+    id: "1",
+    title: "Length",
+    description: "Convert between different length units.",
+    button: "Convert Length",
+  },
+  {
+    id: "2",
+    title: "Weight",
+    description: "Convert between different weight units.",
+    button: "Convert Weight",
+  },
+  {
+    id: "3",
+    title: "Temperature",
+    description: "Convert between different temperature units.",
+    button: "Convert Temperature",
+  },
+  {
+    id: "4",
+    title: "Time",
+    description: "Convert between different time units.",
+    button: "Convert Time",
+  },
+  {
+    id: "5",
+    title: "Speed",
+    description: "Convert between different speed units.",
+    button: "Convert Speed",
+  },
+];
+
+export { unitTypes };
