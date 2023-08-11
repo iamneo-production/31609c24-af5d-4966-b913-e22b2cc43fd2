8d0aef2eeb72b3e4db50628e77cb8e8d30568c12
shivssm <shivssm95@gmail.com>
2023-08-11 12:45:36 +0000

    Added JSON Server & API Setup

--- unit-converter/src/components/mainComponents/lengthConverter/LengthConverter.js
+++ unit-converter/src/components/mainComponents/lengthConverter/LengthConverter.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
+import { fetchLengthUnits, fetchWeightUnits } from "../../../api/Api";
 import "./LengthConverter.css";
 
 const LengthConverter = () => {
+  const [lengthUnits, setLengthUnits] = useState([]);
+  const [weightUnits, setWeightUnits] = useState([]);
+
+  useEffect(() => {
+    fetchLengthUnits()
+      .then(data => {
+        setLengthUnits(data);
+      })
+      .catch(error => {
+      });
+
+
+      fetchWeightUnits()
+      .then(data => {
+        setWeightUnits(data);
+      })
+      .catch(error => {
+      });
+  }, []);
+
   return (
     <div className="app-body">
       <h1>Convert into Length Units</h1>
+      <p>{lengthUnits}</p>
+      {weightUnits}
     </div>
   );
 };
